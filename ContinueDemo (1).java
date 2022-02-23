4309d934740353aca23841ec13d2ab0d0f8c35fe
jithendra arigela <96832918+jithu14@users.noreply.github.com>
2022-02-24 00:02:08 +0530

    Add files via upload

---
+++ ContinueDemo (1).java
@@ -0,0 +1,15 @@
+// Java Program to demonstrate continue in loops.
+
+public class Continue example1 {
+  public static void main(String[] args) {
+      
+  // Running a for loop from 0 to 9	  
+  for (int i = 0; i < 10; i++) {
+    
+  if (i == 5) {
+
+  }
+  System.out.println(i);
+ }
+}
+}
---
+++ Exercise1 (1).java
@@ -0,0 +1,21 @@
+// Write a program to print all odd numbers between 1 to 100 using 'continue'
+
+class Exercise1 {
+
+	public static void main(String args[]) {
+		
+		int numbers=100;
+		system.out.print ("list of odd numbers from 1 to"+numbers":" );
+		for (int i=1;i<=number;i++);
+		{
+			if(i%2!=0)
+			{
+				system.out.print(i+"");
+				
+			}
+		}
+
+
+	}
+
+}
