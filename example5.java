05062184629265835c9d5de3a9a6eb5542bb770c
jithendra arigela <96832918+jithu14@users.noreply.github.com>
2022-02-24 16:37:27 +0530

    Add files via upload

---
+++ example5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+// WAP to accept start and stop values and print between numbers.
+
+class LoopExample{
+    
+    public static void main(String args[]){
+        
+        Scanner s=new Scanner(System.in);
+         int start=0,stop=0;
+         System.out.println("Enter start and stop values");
+         start=s.nextInt();
+         stop=s.nextInt(); 
+         
+         if(start<stop){
+             
+             for(int i=start;i<=stop;i++){
+                 System.out.print(i+" ");
+             }
+         }
+         else if(start>stop){
+             for(int i=start;i>=stop;i--){
+                 System.out.print(i+" ");
+             }
+         }
+         else{
+             System.out.println("Your start and stop values are same");
+         }
+
+    }
+}
