1a7a568971a6b6c621a98c9caa57fec3e0a69399
jithendra arigela <96832918+jithu14@users.noreply.github.com>
2022-02-24 15:05:31 +0530

    Add files via upload

---
+++ example4.java
@@ -0,0 +1,21 @@
+
+
+improtjava.util.scanner;
+
+class Loopexample1{
+    
+    public static void main(String args[]){
+        
+        scanner s=new scanner(system.in);
+        system.out.println("enter n value ");
+        int n=sc.nextint();
+        
+        for(int i=1;i<=n2;i++){
+         
+         if(n%i==0)
+         {
+             count++;
+         }
+        }
+    } 
+}
