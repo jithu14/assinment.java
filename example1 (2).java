bb1c13df196d61c90cbdb08ba47bc951d124b318
jithendra arigela <96832918+jithu14@users.noreply.github.com>
2022-02-24 15:45:12 +0530

    Add files via upload

---
+++ example1 (2).java
@@ -0,0 +1,18 @@
+ import.java.util.scanner
+ // WAP to print 1-n numbers
+class LoopExample{
+    
+    public static void main(String[]){
+        scanner s=new scanner(system.in);
+        
+        system.out.println("enter n value");
+        int n=s.nextInt();  //50
+        
+        int i=0;
+        
+        for (i=1;i<=n;i++){  //i=0 ,1,2
+        system.out.print(i+"");
+        
+    }
+}
+ 
